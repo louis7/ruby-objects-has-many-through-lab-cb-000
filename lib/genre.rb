18bb8500d521ab567234eb72b26c58d9a192769d
louis baptiste <louisvis@ymail.com>
2018-07-14 02:15:47 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,36 +1,24 @@
-require "pry"
-
 
 class Genre
-attr_accessor :name
-@@all = []
-def initialize(name )
-@name = name
-@@all << self
-end
+  attr_accessor :name
 
-def self.all
-  @@all
-end
+  def initialize(name)
+    @name = name
+  end
 
+  def songs
+    Song.all{|song| song.genre == self}
+  end
 
-def new_song(name, genre)
-  @song = Song.new(name, self, genre)
-end
+  def artists
+    songs.map{|song| song.artist}
+  end
 
-def songs
-  Song.all.select { |song| song.genre == self  }
-end
-
-def artists
-songs.map do |song|
-  song.artist.name
+  def add_song(song)
+    self.songs << song
+  end
 
 end
-song.artist
-
-end
-
 
 
 
