52e3146b2148e1048e366e52ee7a164ebd42d0f2
louis baptiste <louisvis@ymail.com>
2018-07-14 01:25:15 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+
+require "pry"
+class Artist
+attr_accessor :name
+@@all = []
+def initialize(name )
+@name = name
+@@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_song(name, genre)
+  @song = Song.new(name, self, genre)
+end
+
+def songs
+  Song.all.select { |song| song.artist == self  }
+end
+
+def genres
+@newgenres = Song.all.collect do |song|
+  if song.genre == self
+     song.genre
+end
+
+end
+return @newgenres
+end
+
+
+
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,33 @@
+class Genre
+attr_accessor :name
+@@all = []
+def initialize(name )
+@name = name
+@@all << self
+end
+
+def self.all
+  @@all
+end
+
+
+def new_song(name, genre)
+  @song = Song.new(name, self, genre)
+end
+
+def songs
+  Song.all.select { |song| song.genre == self  }
+end
+
+def artists
+Song.all.select do |song|
+  song.genre == self
+
+end
+
+end
+
+
+
+
+end
